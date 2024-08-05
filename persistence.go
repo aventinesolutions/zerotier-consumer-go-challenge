445d05ed4610756772cba9012cfacfeb72a6565d
Matthew Evan Eichler <matthew.eichler@aventinesolutions.nl>
2024-08-05 16:57:08 +0200

    :bear: :construction: move Firestore Constants to the Container Environment
    #188034431 As a Developer, I would like to finish the Coding Challenge for ZeroTier, so I can start making an income again.

--- persistence.go
+++ persistence.go
@@ -3,18 +3,18 @@ package main
 import (
 	firestore "cloud.google.com/go/firestore"
 	context "context"
-)
-
-const (
-	GCP_PROJECT   string = "aventine-k8s"
-	DATABASE_NAME string = "zerotier-events"
-	TEST_DOCUMENT string = "aventine/OLU9N1NkR0EYwYpGeBXi"
+	"os"
 )
 
 var ctx = context.Background()
 
+// parameters for Firestore are gotting from the running Container Environment
+var gcp_project_name = os.Getenv("GCP_PROJECT")
+var database_name = os.Getenv("FIRESTORE_DB_NAME")
+var test_document_path = os.Getenv("TEST_DOCUMENT_PATH")
+
 func EventStoreClient() (*firestore.Client, error) {
-	client, err1 := firestore.NewClientWithDatabase(ctx, GCP_PROJECT, DATABASE_NAME)
+	client, err1 := firestore.NewClientWithDatabase(ctx, gcp_project_name, database_name)
 	if err1 != nil {
 		Logger.Errorf("unable to connect to Firestore: %v", err1)
 	}
@@ -31,7 +31,7 @@ func GetDocument(client *firestore.Client, doc_path string) (*firestore.Document
 }
 
 func FetchTestDocument(client *firestore.Client) (*firestore.DocumentSnapshot, error) {
-	doc, err := GetDocument(client, TEST_DOCUMENT)
+	doc, err := GetDocument(client, test_document_path)
 	if err != nil {
 		Logger.Errorf("Error fetching Aventine test document from Firestore: %s", err)
 	}
